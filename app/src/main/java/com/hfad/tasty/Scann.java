3cfbcd376b789adadda5e33aecc44c6b26bd1eb0
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-10-19 21:53:35 +0530

    Update Scann.java

--- app/src/main/java/com/hfad/tasty/Scann.java
+++ app/src/main/java/com/hfad/tasty/Scann.java
@@ -1,144 +1,46 @@
 package com.hfad.tasty;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Vibrator;
 
-import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 
-import java.io.IOException;
-
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 public class Scann extends AppCompatActivity {
 
-    SurfaceView cameraPreview;
-    TextView txtResult;
-    BarcodeDetector barcodeDetector;
-    CameraSource cameraSource;
-    final int RequestCameraPermissionID = 1001;
-      public static String tableid,resid;
-        boolean h;
-        int ia;
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RequestCameraPermissionID: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    try {
-                        cameraSource.start(cameraPreview.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            break;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scann);
-        cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
-        txtResult = (TextView) findViewById(R.id.txtResult);
-        h=true;
-        barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
-        cameraSource = new CameraSource
-                .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1280, 720)
-                .build();
-        //Add Event
-        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    //Request permission
-                    ActivityCompat.requestPermissions(Scann.this,
-                            new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
-                    return;
-                }
-                try {
-                    cameraSource.start(cameraPreview.getHolder());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        setContentView(R.layout.activity_main);
 
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                cameraSource.stop();
-
-            }
-        });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
-                if(qrcodes.size() != 0) {
-                    txtResult.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            //Create vibrate
-                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            if(h){
-                                vibrator.vibrate(140);
-                            }
-                            txtResult.setText(qrcodes.valueAt(0).displayValue);
-                            h=false;
-                        }
-                    });
-                    //show confirm Dialog
-                    String s = txtResult.getText().toString();
-                    validateQRCode(s);
-
-                }
-            }
-        });
-
+        IntentIntegrator intentIntegrator=new IntentIntegrator(MainActivity.this);
+        intentIntegrator.setCaptureActivity(re.class);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setOrientationLocked(true);
+        intentIntegrator.setPrompt("Scanning");
+        intentIntegrator.setBeepEnabled(false);
+        intentIntegrator.setBarcodeImageEnabled(true);
+        intentIntegrator.initiateScan();
     }
 
-    private void validateQRCode(String qrcode) {
-        if (qrcode.startsWith("iScanner@")) {
-            qrcode = qrcode.substring(8);
-            String[] ids = qrcode.split("#");
-             resid = ids[1];
-             tableid = ids[2];
-            Intent rw = new Intent(Scann.this,MainActivity.class);
-            Scann.this.startActivity(rw);
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        IntentResult result=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result!=null && result.getContents()!=null)
+        {
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            intent.putExtra("d",result.getContents());
+            MainActivity.this.startActivity(intent);
         }
 
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
